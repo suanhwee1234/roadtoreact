c0fed8645e66ddd7d1edf8cb84e4e5c6f18cf4e0
suanhwee <suanhwee@gmail.com>
2022-05-16 23:01:36 +0800

    road

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+  // console.log('App renders')
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  //A
+  const [searchTerm, setSearchTerm] = React.useState('React');
+
+  const handleSearch = (event) => {
+
+    setSearchTerm(event.target.value);
+  }
+
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+  console.log(searchedStories)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
     </div>
   );
 }
 
+const List = ({list}) => {
+  // console.log("list renders")
+  return (
+    <ul>
+      {list.map(({objectID,...item}) => (
+        <Item key={item.objectID} {...item} />
+      ))}
+
+
+    </ul>
+  );
+
+}
+
+const Item = ({title,url,author,num_comments,points}) => {
+  // console.log("item renders")
+  return (
+    <li>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
+    </li>
+  )
+}
+
+
+function Search(props) {
+  // console.log("search renders");
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search"
+        type="text"
+        value={props.search}
+        onChange={props.onSearch} />
+
+    </div>
+  );
+}
 export default App;
