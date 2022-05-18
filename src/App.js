bfbe7a64ee49cd87a84bc83296210ab5ee028038
suanhwee <suanhwee@gmail.com>
2022-05-18 22:23:46 +0800

    tut3

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
   // console.log('App renders')
   const stories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
   //A
-  // const [searchTerm, setSearchTerm] = 
+  // const [searchTerm, setSearchTerm] =
   // React.useState(localStorage.getItem('search') || 'React');
 
+  const useSemiPersistanceState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+      localStorage.getItem(key) || initialState
+    );
 
-
-  const useSemiPersistanceState=(key,initialState)=>{
-    const [value, setValue]=
-    React.useState(localStorage.getItem(key)||initialState);
-    
     React.useEffect(() => {
       localStorage.setItem(key, value);
-    }, [value,key]);
-    return [value,setValue];
+    }, [value, key]);
+    return [value, setValue];
   };
-  const [searchTerm, setSearchTerm] = 
-  useSemiPersistanceState('search','React');
-
+  const [searchTerm, setSearchTerm] = useSemiPersistanceState(
+    "search",
+    "React"
+  );
 
   const handleSearch = (event) => {
-
     setSearchTerm(event.target.value);
-
-  }
+  };
 
   const searchedStories = stories.filter(function (story) {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
   });
-  console.log(searchedStories)
+  console.log(searchedStories);
   return (
     <div>
       <h1>My Hacker Stories</h1>
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <Search search={searchTerm} onSearch={handleSearch} />
       <hr />
@@ -63,6 +66,14 @@ function App() {
   );
 }
 
+const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => (
+  <>
+    <label htmlFor="{id}">{label}</label>
+    &nbsp;
+    <input id={id} type={type} value={value} onChange={onInputChange} />
+  </>
+);
+
 const List = ({ list }) => {
   // console.log("list renders")
   return (
@@ -70,12 +81,9 @@ const List = ({ list }) => {
       {list.map(({ objectID, ...item }) => (
         <Item key={item.objectID} {...item} />
       ))}
-
-
     </ul>
   );
-
-}
+};
 
 const Item = ({ title, url, author, num_comments, points }) => {
   // console.log("item renders")
@@ -88,21 +96,17 @@ const Item = ({ title, url, author, num_comments, points }) => {
       <span>{num_comments}</span>
       <span>{points}</span>
     </li>
-  )
-}
-
-
-function Search(props) {
-  // console.log("search renders");
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search"
-        type="text"
-        value={props.search}
-        onChange={props.onSearch} />
-
-    </div>
   );
-}
+};
+
+const Search = ({ search, onSearch }) => (
+  <>
+    <label key="1" htmlFor="search">
+      Search:{" "}
+    </label>
+
+    <input key="2" id="search" type="text" value={search} onChange={onSearch} />
+  </>
+);
+
 export default App;
